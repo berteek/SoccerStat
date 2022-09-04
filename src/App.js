2f13e9ca3218f231b0a3bbc3cc50fbc21b01b4ba
berteek <merzinalbert@gmail.com>
2022-09-04 17:42:52 +0400

    Project clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import { Button } from '@mui/material'
+import React from 'react'
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return <Button></Button>
 }
 
-export default App;
+export default App
