1890ff2af410b3ecd64b37886fe1aa3e1e900240
berteek <merzinalbert@gmail.com>
2022-09-11 16:30:49 +0400

    Routing fix

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -8,6 +8,7 @@ export default function Navigation() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/leagues" />} />
+      <Route path="/soccer-stat" element={<Navigate to="/leagues" />} />
       <Route path="/leagues" element={<Leagues />} />
       <Route path="/teams" element={<Teams />} />
     </Routes>
