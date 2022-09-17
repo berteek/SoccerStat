a392358cb9996dc4f63037e79504423d37e0b8b7
berteek <merzinalbert@gmail.com>
2022-09-17 21:35:55 +0400

    DataPickers implemented

--- src/components/matches/index.js
+++ src/components/matches/index.js
@@ -1,56 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+import dayjs from "dayjs"
+import "dayjs/locale/ru"
 
 import { Box, Stack, TextField, Typography } from "@mui/material"
+
 import { DataGrid } from "@mui/x-data-grid"
+
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers"
+
 import { useGetMatches } from "../../app/repository"
 
-export default function Matches() {
-  const { data, status } = useGetMatches()
-
-  console.log(data)
-
-  if (status === "loading") {
-    return <div>Loading...</div>
-  }
-
-  if (status === "error") {
-    return <div>Error</div>
-  }
-
-  const columns = [
-    {
-      field: "date",
-      flex: 1,
-      headerName: "Дата"
-    },
-    {
-      field: "time",
-      flex: 1,
-      headerName: "Время"
-    },
-    {
-      field: "status",
-      flex: 1,
-      headerName: "Статус матча"
-    },
-    {
-      field: "homeTeam",
-      flex: 1,
-      headerName: "Домашняя команда"
-    },
-    {
-      field: "awayTeam",
-      flex: 1,
-      headerName: "Гостевая команда"
-    },
-    {
-      field: "score",
-      flex: 1,
-      headerName: "Счет"
-    }
-  ]
-
-  const rows = data.matches.map((match, index) => {
+function calculateRows(matches) {
+  return matches.map((match, index) => {
     const dateTime = new Date(Date.parse(match.utcDate))
     const date = dateTime.toLocaleDateString("ru-RU", {
       year: "numeric",
@@ -61,6 +24,7 @@ export default function Matches() {
       hour: "2-digit",
       minute: "2-digit"
     })
+
     let matchStatus = ""
     switch (match.status) {
       case "SCHEDULED":
@@ -90,40 +54,174 @@ export default function Matches() {
       default:
         break
     }
+
+    let scoreFullTime = ""
+    if (match.score.fullTime.homeTeam != null) {
+      scoreFullTime = match.score.fullTime.homeTeam
+        .toString()
+        .concat(":", match.score.fullTime.awayTeam)
+    }
+
+    let scoreExtraTime = ""
+    if (match.score.extraTime.homeTeam != null) {
+      scoreExtraTime = match.score.extraTime.homeTeam
+        .toString()
+        .concat(":", match.score.extraTime.awayTeam)
+    }
+
+    let scorePenalties = ""
+    if (match.score.penalties.homeTeam != null) {
+      scorePenalties = match.score.penalties.homeTeam
+        .toString()
+        .concat(":", match.score.penalties.awayTeam)
+    }
+
     return {
       id: index,
       date,
       time,
       status: matchStatus,
       homeTeam: match.homeTeam.name,
-      awayTeam: match.awayTeam.name
+      awayTeam: match.awayTeam.name,
+      scoreFullTime,
+      scoreExtraTime,
+      scorePenalties
     }
   })
+}
 
-  console.log(rows)
+export default function Matches() {
+  const { data, status } = useGetMatches()
+
+  const [matches, setMatches] = useState([])
+  const [rows, setRows] = useState([])
+
+  const [dateFrom, setDateFrom] = useState(dayjs("1995-12-17T03:24:00"))
+  const [dateTo, setDateTo] = useState(dayjs("1995-12-17T03:24:00"))
+
+  useEffect(() => {
+    if (matches.length !== 0) {
+      setRows(calculateRows(matches))
+      if (dateFrom.valueOf() === dayjs("1995-12-17T03:24:00").valueOf()) {
+        setDateFrom(dayjs(matches[0].utcDate))
+        setDateTo(dayjs(matches[matches.length - 1].utcDate))
+      }
+    }
+  }, [matches])
+
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
+
+  if (status === "error") {
+    return <div>Error</div>
+  }
+
+  if (matches.length === 0) {
+    setMatches(data.matches)
+  }
+
+  const columns = [
+    {
+      field: "date",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Дата"
+    },
+    {
+      field: "time",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Время"
+    },
+    {
+      field: "status",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Статус матча"
+    },
+    {
+      field: "homeTeam",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Домашняя команда"
+    },
+    {
+      field: "awayTeam",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Гостевая команда"
+    },
+    {
+      field: "scoreFullTime",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Основное время"
+    },
+    {
+      field: "scoreExtraTime",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Доп. время"
+    },
+    {
+      field: "scorePenalties",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      headerName: "Пенальти"
+    }
+  ]
 
   return (
     <Stack spacing={2} margin={4}>
       <Typography variant="h5">Матчи</Typography>
       <Stack direction="row" alignItems="center" spacing={2}>
-        <Typography variant="p">с</Typography>
-        <TextField
-          id="date"
-          type="date"
-          size="small"
-          InputLabelProps={{
-            shrink: true
-          }}
-        />
-        <Typography variant="p">по</Typography>
-        <TextField
-          id="date"
-          type="date"
-          size="small"
-          InputLabelProps={{
-            shrink: true
-          }}
-        />
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+          <Typography variant="p">с</Typography>
+          <DesktopDatePicker
+            size="small"
+            value={dateFrom}
+            onChange={(newDateFrom) => {
+              setDateFrom(newDateFrom)
+              setMatches(
+                data.matches.filter((match) => {
+                  const unixTime = Date.parse(match.utcDate)
+                  return (
+                    unixTime >= newDateFrom.valueOf() &&
+                    unixTime <= dateTo.valueOf()
+                  )
+                })
+              )
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <Typography variant="p">по</Typography>
+          <DesktopDatePicker
+            size="small"
+            value={dateTo}
+            onChange={(newDateTo) => {
+              setDateTo(newDateTo)
+              setMatches(
+                data.matches.filter((match) => {
+                  const unixTime = Date.parse(match.utcDate)
+                  return (
+                    unixTime <= newDateTo.valueOf() &&
+                    unixTime >= dateFrom.valueOf()
+                  )
+                })
+              )
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
       </Stack>
       <Box sx={{ height: 400, width: "100%" }}>
         <DataGrid
